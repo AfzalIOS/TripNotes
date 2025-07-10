d70634c202282e69f859a879be2d8593e0d5208b
afzal_ahmed <ahmed.iosdev101@gmail.com>
2025-07-10 21:21:32 +0500

    Initial Commit

---
+++ TripNotes/TripNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripNotesApp.swift
+//  TripNotes
+//
+//  Created by Samoo on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TripNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
